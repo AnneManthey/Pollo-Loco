bbfecc13b1449e79d06c2073bde12270b5425a82
Anne Manthey <anne.manthey@pm.me>
2026-06-18 08:35:00 +0200

    feat: add classes character, chicken, world

---
+++ js/models/character.class.js
@@ -0,0 +1,5 @@
+class Charakter extends MovableObject {
+
+
+    jump(){}
+}
---
+++ js/models/world.class.js
@@ -0,0 +1,12 @@
+class World {
+
+    character = new Charakter();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken()
+    ];
+
+
+    draw() { }
+}
