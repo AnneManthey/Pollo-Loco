4e1e1607f994709d0b75d4fe352f9cc94c10af85
Anne Manthey <anne.manthey@pm.me>
2026-06-18 14:10:29 +0200

    refactor: class world/draw (merge/outsource functions)

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -8,10 +8,13 @@ class World {
     ];
     clouds = [
         new Cloud()
-    ]
+    ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png'),
+    ];
     canvas;
     ctx;
-    constructor(canvas){
+    constructor(canvas) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
         this.draw();
@@ -21,20 +24,30 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
+        this.addToMap(this.character);
 
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.backgroundObjects);
 
 
         // Draw wird immer wieder aufgerufen (soviele FPS, wie die Grafikkarte hergibt)
         let self = this;
-        requestAnimationFrame(function(){
+        requestAnimationFrame(function () {
             self.draw();
         });
-     }
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+
+        })
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    }
+
+
 }
