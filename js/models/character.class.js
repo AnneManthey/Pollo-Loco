4b083256eb6f244efb270ed698ba6f2861dc3e9a
Anne Manthey <anne.manthey@pm.me>
2026-06-19 07:05:34 +0200

    refactor: adjust size (character, chickens)

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,5 +1,9 @@
 class Charakter extends MovableObject {
 
+    y = 150
+    height = 300;
+    width = 130;
+
     constructor(){
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -1,6 +1,8 @@
 class Chicken extends MovableObject {
 
-    y = 300;
+    y = 360;
+    height = 80;
+    width = 50;
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
