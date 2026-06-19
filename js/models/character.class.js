e023f201a471e0921bb265ed7d0b3ecf7a3e5464
Anne Manthey <anne.manthey@pm.me>
2026-06-19 07:53:39 +0200

    fead: add animate (character)

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -3,10 +3,32 @@ class Charakter extends MovableObject {
     y = 150
     height = 300;
     width = 130;
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ]
+    currentImage = 0;
 
-    constructor(){
+    constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
     }
 
-    jump(){}
+    animate() {
+
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length; // modulo (%): i = 0, 1, 2, 3, 4, 5, 6, 0, 1, ...
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100)
+
+    }
+
+    jump() { }
 }
