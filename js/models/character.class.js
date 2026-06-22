c80e7c811bfeeac21d3ea04b228349a772fb067c
Anne Manthey <anne.manthey@pm.me>
2026-06-22 06:23:33 +0200

    feat: add move character (add to animate)

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
     y = 150
     height = 300;
     width = 130;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -23,14 +24,26 @@ class Character extends MovableObject {
     animate() {
 
         setInterval(() => {
-            if(this.world.keyboard.RIGHT){
+             if(this.world.keyboard.RIGHT){
+                 this.x += this.speed;
+             }
+
+             if(this.world.keyboard.LEFT){
+                 this.x -= this.speed;
+             }
+        }, 1000/60);
+
+        setInterval(() => {
+            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT ){
+               
             
+            // walk animation    
             let i = this.currentImage % this.IMAGES_WALKING.length; // modulo (%): i = 0, 1, 2, 3, 4, 5, 6, 0, 1, ...
             let path = this.IMAGES_WALKING[i];
             this.img = this.imageCache[path];
             this.currentImage++;
             }
-        }, 150)
+        }, 50)
     }
 
     jump() { }
