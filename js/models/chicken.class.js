f5c5753f540cedc9e7e5ce4d976ba87c4ae2438c
Anne Manthey <anne.manthey@pm.me>
2026-06-24 06:56:52 +0200

    feat: add to animate (chicken): moving back and forward on set x-area

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -8,19 +8,32 @@ class Chicken extends MovableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ]
+    minX = 300;
+    maxX = 1000;
+    movingLeft = true;
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
-        this.x = 200 + Math.random() * 500; // Zahl zwischen 200 und 700
+        this.x = this.minX + Math.random() * (this.maxX - this.minX); // Startposition zufällig zwischen minX und maxX
         this.speed = 0.15 + Math.random() * 0.25;
         this.animate();
     }
 
     animate() {
         setInterval(() => {
-            if (this.x > 300){
+            if (this.movingLeft){
             this.moveLeft();
+            if (this.x <= this.minX){
+                this.movingLeft = false;
+                this.otherDirection = true;
+            }
+            } else {
+                this.moveRight();
+                if (this.x >= this.maxX){
+                    this.movingLeft = true;
+                    this.otherDirection = false;
+                }
             }
             
         }, 1000 / 60);
